2ddc04aab509f8860ff9995b8ff90965d3910833
alicfeng <a@samego.com>
2020-02-25 19:42:08 +0800

    feature:migrate:schema

---
+++ src/Schema/Grammars/MySqlGrammar.php
@@ -0,0 +1,47 @@
+<?php
+
+/*
+ * What samego team is that is 'one thing, a team, work together'
+ * Value comes from technology, technology comes from sharing~
+ * https://github.com/alicfeng/laravel-migrate
+ * AlicFeng | a@samego.com
+ */
+
+namespace Samego\Migrate\Schema\Grammars;
+
+use Illuminate\Database\Connection;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Schema\Grammars\MySqlGrammar as BaseMySqlGrammar;
+use Illuminate\Support\Fluent;
+
+/**
+ * Class MySqlGrammar.
+ * @version 1.0.0
+ * @author  AlicFeng
+ */
+class MySqlGrammar extends BaseMySqlGrammar
+{
+    /**
+     * @function    compileCreate
+     * @description rewrite func for supported setting comment as well as auto_increment
+     * @return string
+     * @author      AlicFeng
+     */
+    public function compileCreate(Blueprint $blueprint, Fluent $command, Connection $connection)
+    {
+        $sql = parent::compileCreate($blueprint, $command, $connection);
+
+        // 1. supported table comment
+        if (isset($blueprint->comment)) {
+            $blueprint->comment = str_replace("'", "\'", trim($blueprint->comment));
+            $sql                .= " comment = '{$blueprint->comment}'";
+        }
+
+        // 2. supported field auto_increment value setting
+        if (isset($blueprint->auto_increment) && is_numeric($blueprint->auto_increment)) {
+            $sql .= " auto_increment = {$blueprint->auto_increment}";
+        }
+
+        return $sql;
+    }
+}
---
+++ src/Schema/Schema.php
@@ -0,0 +1,70 @@
+<?php
+
+/*
+ * What samego team is that is 'one thing, a team, work together'
+ * Value comes from technology, technology comes from sharing~
+ * https://github.com/alicfeng/laravel-migrate
+ * AlicFeng | a@samego.com
+ */
+
+namespace Samego\Migrate\Schema;
+
+use Illuminate\Database\Connection;
+use Illuminate\Database\Schema\Builder;
+use Illuminate\Support\Facades\Facade;
+use Samego\Migrate\Schema\Grammars\MySqlGrammar;
+
+/**
+ * @method static \Illuminate\Database\Schema\Builder create(string $table, \Closure $callback)
+ * @method static \Illuminate\Database\Schema\Builder drop(string $table)
+ * @method static \Illuminate\Database\Schema\Builder dropIfExists(string $table)
+ * @method static \Illuminate\Database\Schema\Builder table(string $table, \Closure $callback)
+ *
+ * @see \Illuminate\Database\Schema\Builder
+ */
+class Schema extends Facade
+{
+    /**
+     * Get a schema builder instance for a connection.
+     *
+     * @param string $name
+     * @return Builder
+     */
+    public static function connection($name)
+    {
+        $connection = static::$app['db']->connection($name);
+
+        return static::customGrammar($connection);
+    }
+
+    /**
+     * Get a schema builder instance for the default connection.
+     *
+     * @return \Illuminate\Database\Schema\Builder
+     */
+    protected static function getFacadeAccessor()
+    {
+        /**
+         * @var Connection
+         */
+        $connection = static::$app['db']->connection();
+
+        return static::customGrammar($connection);
+    }
+
+    /**
+     * load custom MySqlGrammar when using MySqlConnection.
+     * @param $connection
+     * @return \Illuminate\Database\Schema\Builder
+     */
+    private static function customGrammar(Connection $connection)
+    {
+        // only for MySQL connection
+        if ('Illuminate\\Database\\MySqlConnection' === get_class($connection)) {
+            $grammar = $connection->withTablePrefix(new MySqlGrammar());
+            $connection->setSchemaGrammar($grammar);
+        }
+
+        return $connection->getSchemaBuilder();
+    }
+}
